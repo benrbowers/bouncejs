8f485a499361cd7f98aedff452caace3c0eb6baf
Ben Bowers <benjambowers@gmail.com>
2020-03-07 00:34:17 +0000

    files

--- test.js
+++ test.js
@@ -1,30 +1,6 @@
 import { Vector2 } from './Vector2.js';
-import { PhysObject } from './PhysObject.js';
+import { Ball } from './Ball.js';
 
-var vector = new Vector2(1, 0);
-console.log(vector.direction * 180 / Math.PI);//0
+while(true) {
 
-vector.y = 1;
-console.log(vector.direction * 180 / Math.PI);//45
-
-vector.x = 0;
-console.log(vector.direction * 180 / Math.PI);//90
-
-vector.x = -1;
-console.log(vector.direction * 180 / Math.PI);//135
-
-vector.y = 0;
-console.log(vector.direction * 180 / Math.PI);//180
-
-vector.y = -1;
-console.log(vector.direction * 180 / Math.PI);//225
-
-vector.x = 0;
-console.log(vector.direction * 180 / Math.PI);//270
-
-vector.x = 1;
-console.log(vector.direction * 180 / Math.PI);//315
-
-vector.y = 0;
-vector.x = 0;
-console.log(vector.direction * 180 / Math.PI);
+}
